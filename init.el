3005f95c4023f47f61f8dcfbb5dd55b90d253aa8
Matthew Sullivan <elephant454@gmail.com>
2017-01-27 15:55:22 -0800

    added more stuff for choosing directories

--- init.el
+++ init.el
@@ -275,7 +275,9 @@ Lisp function does not specify a special indentation."
                                                 ; leader key later
 
 ;; do some calculations to figure out where directories should be
-(setq elephant454initel-current-year (format-time-string "%Y"))
+(setq elephant454initel-current-year (nth 5 (decode-time)))
+(setq elephant454initel-current-month (nth 4 (decode-time)))
+(setq elephant454initel-is-second-semester (< elephant454initel-current-month 8))
 
 ;; in order to figure out how binding keys works, I'm going to need
 ;; this page:
@@ -698,7 +700,6 @@ Lisp function does not specify a special indentation."
     ("d600c677f1777c1e4bfb066529b5b73c0179d0499dd4ffa3f599a0fb0cfbd501" "7e376fb329a0e46a04e8285b0e45199a083f98c69b0e1039ec1cb1d366e66e9c" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "b747fb36e99bc7f497248eafd6e32b45613ee086da74d1d92a8da59d37b9a829" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "abe5ee8858cd1fbe36304a8c3b2315d3e0a4ef7c8588fcc45d1c23eafb725bb6" default)))
  '(debug-on-error t)
  '(erc-autojoin-mode t)
- ;;'(fci-rule-color "#515151")
  '(midnight-mode t)
  '(minimap-mode t)
  '(org-agenda-files
