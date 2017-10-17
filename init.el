3efcfe1b56711c3eb1cb945258b4f6ebb62c949d
Matthew Sullivan <elephant454@gmail.com>
2017-10-16 22:23:50 -0700

    commented out using google chrome as the default browser

--- init.el
+++ init.el
@@ -35,6 +35,7 @@
  ;;browse-url-browser-function 'browse-url-generic
  browse-url-browser-function 'eww-browse-url
  browse-url-generic-program "google-chrome-stable"
+ ;;browse-url-generic-program "google-chrome-stable"
 
  ;; start debugging when something signals an error
  debug-on-error t
