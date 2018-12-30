fbb8bc86f6253ed41ba7b8352ee7d3e8b53a28ef
Matthew Sullivan <elephant454@gmail.com>
2018-12-29 22:24:11 -0800

    tried to create a quick function to disable-theme on all of the themes I have, but it doesn't really work as far as avoiding weird overlapping themes

--- init.el
+++ init.el
@@ -260,6 +260,15 @@ without confirmation."
 
 ;;(e454iel-load-theme)
 
+;; This doesn't seem to work? 😅
+(defun e454iel-disable-all-theme-pairs ()
+  "Run `disable-theme' on every theme in `e454iel-theme-pairs'."
+  (mapc
+   (lambda (x)
+     (print (car x))
+     (print (cdr x)))
+   e454iel-theme-pairs))
+
 ;; load default theme
 (e454iel-jump-to-theme 'silkworm)
 
