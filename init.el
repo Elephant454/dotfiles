2ac28c330a1d335ddcd2e5741dec25a69ff66705
Matthew Sullivan <elephant454@gmail.com>
2020-10-18 15:47:27 -0700

    corrected a theme pairing to be better complements

--- init.el
+++ init.el
@@ -213,7 +213,7 @@ Lists in `LISTS' that are not lists will be listified by `listify'."
 (setq e454iel-theme-pairs '((soft-morning . omtose-softer)
                             (silkworm . foggy-night)
                             (gruvbox-light-hard . gruvbox-dark-hard)
-                            (kaolin-light . kaolin-eclipse)
+                            (kaolin-light . kaolin-dark)
                             (doom-one . doom-one)
                             (doom-fairy-floss . doom-laserwave)
                             (doom-opera-light . doom-opera)
