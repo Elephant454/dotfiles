481eee3c666593cb99968670aad1b9186a1da814
Matthew Sullivan <elephant454@gmail.com>
2020-11-11 17:47:28 -0800

    added tons of EXWM configuration

--- init.el
+++ init.el
@@ -769,14 +769,147 @@ _-_increase _=_decrease"
 
 ;; just for the heck of it 
 (use-package exwm
-  :disabled
-  :config (setq exwm-randr-workspace-output-plist
-                '(0 "HDMI-0"
-                    1 "DVI-D-0"
-                    2 "HDMI-0"
-                    3 "HDMI-0"
-                    4 "HDMI-0"
-                    5 "DVI-D-0")))
+  ;;:disabled
+  ;;:straight (exwm :type built-in)
+  :config
+  (progn
+    (use-package exwm-config :straight (exwm-config :type built-in))
+    (use-package exwm-randr :straight (exwm-config :type built-in))
+
+    ;; TODO: Set my workspaces, displays, and resolution/refresh rate (through
+    ;;  external xrandr commands) based on the current computer (based on the
+    ;;  value of the hostname)
+
+    (setq exwm-randr-workspace-output-plist
+          '(0 "DisplayPort-1"
+              1 "HDMI-A-0"
+              2 "DisplayPort-1"
+              3 "DisplayPort-1"
+              4 "DisplayPort-1"
+              5 "DisplayPort-1"))
+    (exwm-randr-enable)
+
+    ;;(exwm-config-default)
+
+    ;; Here's the start of a lot of miscellaneous configuration stuff based on
+    ;;  pieces of defaults I don't understand. It's from `exwm-config-example'
+
+    ;; Set the initial workspace number.
+    (unless (get 'exwm-workspace-number 'saved-value)
+      (setq exwm-workspace-number 4))
+    ;; Make class name the buffer name
+    (add-hook 'exwm-update-class-hook
+              (lambda ()
+                (exwm-workspace-rename-buffer exwm-class-name)))
+    ;; Global keybindings.
+    (unless (get 'exwm-input-global-keys 'saved-value)
+      (setq exwm-input-global-keys
+            `(
+              ;; 's-r': Reset (to line-mode).
+              ([?\s-r] . exwm-reset)
+              ;; 's-w': Switch workspace.
+              ([?\s-w] . exwm-workspace-switch)
+              ;; 's-&': Launch application.
+              ([?\s-&] . (lambda (command)
+                           (interactive (list (read-shell-command "$ ")))
+                           (start-process-shell-command command nil command)))
+              ;; 's-N': Switch to certain workspace.
+              ,@(mapcar (lambda (i)
+                          `(,(kbd (format "s-%d" i)) .
+                            (lambda ()
+                              (interactive)
+                              (exwm-workspace-switch-create ,i))))
+                        (number-sequence 0 9)))))
+    ;; Line-editing shortcuts
+    (unless (get 'exwm-input-simulation-keys 'saved-value)
+      (setq exwm-input-simulation-keys
+            '(([?\C-b] . [left])
+              ([?\C-f] . [right])
+              ([?\C-p] . [up])
+              ([?\C-n] . [down])
+              ([?\C-a] . [home])
+              ([?\C-e] . [end])
+              ([?\M-v] . [prior])
+              ([?\C-v] . [next])
+              ([?\C-d] . [delete])
+              ([?\C-k] . [S-end delete]))))
+    ;; Enable EXWM
+    (exwm-enable)
+    ;; Other configurations
+    (exwm-config-misc)
+
+    ;; Some launches I usually do with my window manager
+    (start-process-shell-command "syncthing"
+                                 nil
+                                 "syncthing -no-browser -no-restart -logflags 0")
+    (start-process-shell-command "mpd" nil "mpd")
+    (start-process-shell-command "redshift"
+                                 nil
+                                 (concat
+                                  "redshift -l "
+                                  (number-to-string calendar-latitude)
+                                  ":"
+                                  (number-to-string calendar-longitude)
+                                  " -t 6500:3000"))
+    (start-process-shell-command "dunst"
+                                 nil
+                                 "dunst")
+    (start-process-shell-command "compton"
+                                 nil
+                                 "compton")
+    (start-process-shell-command  "feh"
+                                  nil
+                                  "~/.fehbg")
+    (start-process-shell-command "compose:caps"
+                                 nil
+                                 "setxkbmap -option compose:caps")
+    (start-process-shell-command "shifts_toggle"
+                                 nil
+                                 "setxkbmap -option grp:shifts_toggle")
+    (start-process-shell-command "setxkbmap"
+                                 nil
+                                 "setxkbmap us,gr")
+    (start-process-shell-command "xrandr"
+                                 nil
+                                 (concat
+                                  "xrandr"
+                                  " --output HDMI-A-0"
+                                  " --mode 1920x1080"
+                                  " --rate 60.00"
+                                  " --output DisplayPort-1"
+                                  " --mode 2560x1440"
+                                  " --rate 143.96"
+                                  " --left-of HDMI-A-0"))
+
+    ;; Make C-SPC open the main menu anywhere
+    (push
+     (cons (kbd "<C-SPC>") e454iel-main-menu-prefix)
+     exwm-input-global-keys)
+
+    ;; So long as I keep the original workspaces empty-ish, this is essentially
+    ;;  equivalent to "show desktop"
+    (defun e454iel-jump-to-original-workspace ()
+      (interactive)
+      (if (> (length exwm-randr-workspace-monitor-plist) 2)
+          (progn 
+          (exwm-workspace-switch-create
+           (third exwm-randr-workspace-output-plist))
+
+          (eyebrowse-switch-to-window-config-1)))
+
+      (exwm-workspace-switch-create
+       (first exwm-randr-workspace-output-plist))
+
+      (eyebrowse-switch-to-window-config-1))
+
+    (push
+     (cons (kbd "<s-escape>") #'e454iel-jump-to-original-workspace)
+     exwm-input-global-keys)
+
+    ))
+
+(use-package dmenu
+  :config (e454iel-main-menu "ad" 'dmenu))
 
 (defun directory-directories (directory &optional full match nosort)
   "Find the sub-directories in `DIRECTORY'.
