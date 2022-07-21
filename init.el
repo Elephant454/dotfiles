7f39237f2e03ca23c12d53ad6fe4517093737ccf
elephant454@gmail.com <elephant454@gmail.com>
2022-07-20 21:14:37 -0700

    changed the holiday symbol to be more "summery"

--- init.el
+++ init.el
@@ -2143,8 +2143,8 @@ Lisp function does not specify a special indentation."
             (setq display-time-interval 0.95)
             ;; TODO: Pi day, Pride month, December, Fall, Halloween, Thanksgiving week, Birthday
             ;; TODO: Maybe I could have the cake show every time it's the birthday of someone I know?
-            (setq e454iel-holiday-symbol "🏳️‍🌈")
-            (setq display-time-format (concat "%F %H:%M:%S " e454iel-holiday-symbol))
+            (setq e454iel-holiday-symbol "🌴🐚🍹")
+            (setq display-time-format (concat "%F %H:%M:%S %a " e454iel-holiday-symbol))
             (display-time-mode t)))
 
 ;; used to center buffers in the middle of the screen
