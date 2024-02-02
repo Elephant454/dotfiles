baf88fcf4f420ab79dce0bced000139beb03989c
elephant454@gmail.com <elephant454@gmail.com>
2024-02-02 12:59:01 -0800

    Set the holiday symbol to be Valentine's Day (I'm at a loss for what to set it to right now)

--- init.el
+++ init.el
@@ -2713,7 +2713,7 @@ Lisp function does not specify a special indentation."
             (setq display-time-interval 0.95)
             ;; TODO: Pi day, Pride month, December, Fall, Halloween, Thanksgiving week, Birthday
             ;; TODO: Maybe I could have the cake show every time it's the birthday of someone I know?
-            (setq e454iel-holiday-symbol "🎃🕯️🦃🍂")
+            (setq e454iel-holiday-symbol "❤️🕯️🍫❤️")
             (setq display-time-format (concat "%F %H:%M:%S %a " e454iel-holiday-symbol))
             (display-time-mode t)))
 
